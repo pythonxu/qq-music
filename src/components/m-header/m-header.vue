3f29111691b05e3c7ada228ce773c012308ddff4
SDXuJun <1139809887@qq.com>
2017-08-03 20:23:56 +0800

    添加初始化文件

--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -1,15 +1,11 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div>
+
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-export default {
-  name: 'app'
-}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
 </style>
