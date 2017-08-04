b927994e4530be7a2b6c7019c5126436b7394443
SDXuJun <1139809887@qq.com>
2017-08-04 23:52:59 +0800

    添加轮播效果

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -4,6 +4,9 @@
       <slot>
       </slot>
     </div>
+    <div class="dots">
+      <span class="dot" v-for="(item, index) in dots" :class="{active: index === currentPageIndex}"></span>
+    </div>
   </div>
 </template>
 
@@ -36,13 +39,17 @@
     mounted() {
       setTimeout(() => {
         this._setSliderWidth()
+        this._initDots()
         this._initSlider()
+        if (this.autoPlay) this._play()
       }, 20)
     },
     methods: {
+      _initDots() {
+        this.dots = new Array(this.children.length)
+      },
       _setSliderWidth() {
         this.children = this.$refs.sliderGroup.children
-
         let width = 0
         let sliderWidth = this.$refs.slider.clientWidth
         for (let i = 0; i < this.children.length; i++) {
@@ -65,6 +72,25 @@
           snapThreshold: 0.3,
           snapSpeed: 400
         })
+
+        this.slider.on('scrollEnd', () => {
+          let pageIndex = this.slider.getCurrentPage().pageX
+          // 自动轮播模式，BScroll会前后多加两个元素，所以-1
+          if (this.loop) pageIndex -= 1
+          this.currentPageIndex = pageIndex
+
+          if (this.autoPlay) {
+            clearTimeout(this.timer)
+            this._play()
+          }
+        })
+      },
+      _play() {
+        let pageIndex = this.currentPageIndex + 1
+        if (this.loop) pageIndex += 1
+        this.timer = setTimeout(() => {
+          this.slider.goToPage(pageIndex,0,400)
+        },this.interval)
       }
     }
   }
@@ -92,4 +118,22 @@
         img
           display: block
           width: 100%
+    .dots
+      position: absolute
+      right: 0
+      left: 0
+      bottom: 12px
+      text-align: center
+      font-size: 0
+      .dot
+        display: inline-block
+        margin: 0 4px
+        width: 8px
+        height: 8px
+        border-radius: 50%
+        background: $color-text-l
+        &.active
+          width: 20px
+          border-radius: 5px
+          background: $color-text-ll
 </style>
