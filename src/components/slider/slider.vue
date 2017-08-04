3557805eda6570123f1d193d13b9a5c904036674
fandanmac <Levi.li@fandine.com>
2017-08-04 17:18:09 +0800

    添加better-scroll

--- src/components/slider/slider.vue
+++ src/components/slider/slider.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="slider">
 
+
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  export default {
+    data() {
+
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
