2bc39947c3a0c3acd080e53129e3c1b541b9079a
fandanmac <Levi.li@fandine.com>
2017-08-04 12:01:27 +0800

    安装jsonp

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-    recommend
+  <div class="recommend">
+    <div class="slider-wrapper">
+      <slider>
+
+      </slider>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Slider from 'components/slider/slider'
+
+  export default {
+    components: {
+      Slider
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
