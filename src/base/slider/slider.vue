bb0c931bf9fd4122cc32d2573237da99ed249d02
SDXuJun <1139809887@qq.com>
2017-08-04 22:29:32 +0800

    设置slider宽度  和group的宽度

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="slider">
-    <div class="slider-group">
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
       <slot>
       </slot>
     </div>
@@ -8,13 +8,78 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import {addClass} from 'common/js/dom'
   export default {
+    props: {
+      loop: {
+        type: Boolean,
+        default: true
+      },
+      autoPlay: {
+        type: Boolean,
+        default: true
+      },
+      interval: {
+        type: Number,
+        default: 4000
+      }
+    },
     data() {
+      return {
+        dots: [],
+        currentPageIndex: 0
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._setSliderWidth()
+        this._initSlider()
+      }, 20)
+    },
+    methods: {
+      _setSliderWidth() {
+        this.children = this.$refs.sliderGroup.children
 
+        let width = 0
+        let sliderWidth = this.$refs.slider.clientWidth
+        for (let i = 0; i < this.children.length; i++) {
+          let child = this.children[i]
+          addClass(child, 'slider-item')
+          child.style.width = sliderWidth + 'px'
+          width += sliderWidth
+        }
+
+        if (this.loop) width += 2 * sliderWidth
+        this.$refs.sliderGroup.style.width = width + 'px'
+      },
+      _initSlider() {
+
+      }
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .slider
+    min-height: 1px
+    .slider-group
+      position: relative
+      overflow: hidden
+      white-space: nowrap
+      .slider-item
+        float: left
+        box-sizing: border-box
+        overflow: hidden
+        text-align: center
+        a
+          display: block
+          width: 100%
+          overflow: hidden
+          text-decoration: none
+        img
+          display: block
+          width: 100%
 
 </style>
--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -1,1 +1,11 @@
+export function addClass(el, className) {
+  if (hasClass(el,className)) return
+  let newClassName = el.className.split(' ')
+  newClassName.push(className)
+  el.className = newClassName.join(' ')
+}
 
+export function hasClass(el, className) {
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+  return reg.test(el.className)
+}
