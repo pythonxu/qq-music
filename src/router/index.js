7e42f82ea53d0a86520866f8367f2ac03d34d745
fandanmac <Levi.li@fandine.com>
2017-08-04 11:36:43 +0800

    完成Tab组件UI

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Recommend from 'components/recommend/recommend'
+import Singer from 'components/singer/singer'
+import Rank from 'components/rank/rank'
+import Search from 'components/search/search'
 
 Vue.use(Router)
 
@@ -13,6 +16,18 @@ export default new Router({
     {
       path: '/recommend',
       component: Recommend
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/singer',
+      component: Singer
+    },
+    {
+      path: '/rank',
+      component: Rank
     }
   ]
 })
