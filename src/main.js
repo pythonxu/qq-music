12a8d981dc9aafe46eddc5ede58978834b25a41c
SDXuJun <1139809887@qq.com>
2017-08-05 21:33:30 +0800

    使用v-lazyload

--- src/main.js
+++ src/main.js
@@ -3,14 +3,18 @@ import fastclick from 'fastclick'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-Vue.config.productionTip = false
+import VueLazyload from 'vue-lazyload'
+// 把一些基础样式导入
+import 'common/stylus/index.styl'
 
 // 移动端点击300毫秒延时解决
 fastclick.attach(document.body)
 
-// 把一些基础样式导入
-import 'common/stylus/index.styl'
+Vue.use(VueLazyload,{
+  loading: require('common/images/default.png')
+})
+
+
 
 /* eslint-disable no-new */
 new Vue({
