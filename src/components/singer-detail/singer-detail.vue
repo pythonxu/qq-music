9a80cf8fbf4e2e84db4fe849388b8dd9a6dd788d
fandanmac <Levi.li@fandine.com>
2017-08-07 13:23:18 +0800

    歌曲列表页面UI

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,12 +1,13 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
   import {mapGetters} from 'vuex'
   import {getSingerDetail} from 'api/singer.js'
+  import MusicList from 'components/music-list/music-list'
   import {createSong} from 'common/js/song'
   import {ERR_OK} from 'api/config'
 
@@ -19,7 +20,13 @@
     computed: {
       ...mapGetters([
         'singer'    // 在getter.js中做了挂载
-      ])
+      ]),
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      }
     },
     created() {
       this._getSingerDetail()
@@ -46,21 +53,15 @@
         })
         return ret
       }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    right: 0
-    left: 0
-    bottom: 0
-    background: $color-background
 
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
