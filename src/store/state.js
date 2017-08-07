2e44d9fc8e9c16c7ed6f019bed6e21c4a40c3cbd
SDXuJun <1139809887@qq.com>
2017-08-07 19:03:46 +0800

    设计数据 mutations

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,11 @@
 const state = {
-  singer: {}
+  singer: {},
+  playing: false,
+  fullScreen: false,
+  playlist: [],
+  sequenceList: [],
+  mode: playMode.sequence,
+  currentIndex: -1
 }
 
 export default state
