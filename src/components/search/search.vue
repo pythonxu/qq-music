c0c9b79a40912b3d82bd873d1cb5717b67c84494
SDXuJun <1139809887@qq.com>
2017-08-09 20:40:37 +0800

    添加search-box组件

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>
-    search
+  <div class="search">
+    <div class="search-box-wrapper">
+      <search-box></search-box>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import SearchBox from 'base/search-box/search-box'
+
+  export default {
+    components: {
+      SearchBox
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  .search
+    .search-box-wrapper
+      margin: 20px
 </style>
